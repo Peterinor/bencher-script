464df0a8b84388aa852cec8c7f85a15411fca3dd
yu.tang <yu.tang@hotmail.com>
2018-12-06 22:42:51 +0800

    win command tpl

--- lib/url-cmd-tpl@ab.bat
+++ lib/url-cmd-tpl@ab.bat
@@ -3,7 +3,7 @@ if not exist {{=it.out_dir}} mkdir {{=it.out_dir.replace(/\//g, '\\')}}
 if not exist {{=it.out_dir}}\\data mkdir {{=it.out_dir.replace(/\//g, '\\')}}\\data
 {{~it.conc_list:i}}{{var count = i * 50; if(count < 250) count = 250; }}
 echo benching with ab -n {{=count}} -c {{=i}} {{=it.url.replace(/&/g, '^&')}}
-ab -n {{=count}} -c {{=i}} -g {{=it.out_dir}}/data/{{=it.group}}-{{=it.action}}-$i.dat ^
+ab -n {{=count}} -c {{=i}} -g {{=it.out_dir}}/data/{{=it.group}}-{{=it.action}}-{{=i}}.dat ^
     {{~it.headers:header}} -H "{{=header}}"{{~}} {{?it.contentType}}-T "{{=it.contentType}}"{{?}} ^
     {{?it.postfile}}-p "{{=it.postfile}}"{{?}} "{{=it.url}}"  >  {{=it.out_dir}}/{{=it.group}}-{{=it.action}}-{{=i}}.txt
 {{~}}
